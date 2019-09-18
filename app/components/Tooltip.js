a7aaffe310f14a1f5809c20812e27ac7daf8b836
BarbaraBB <barbarabudzyk@gmail.com>
2019-09-18 18:30:54 +0200

    refactoring - applied render prop pattern

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import withHover from './withHover'
+import Hover from './Hover'
 
 const styles = {
   container: {
@@ -24,13 +24,17 @@ const styles = {
   }
 }
 
-function Tooltip({text, children, hovering}){
+export default function Tooltip({text, children}){
     return(
-        <div
-            style={styles.container}>
-                {hovering === true && <div style={styles.tooltip}>{text}</div>}
-                {children}
-        </div>
+        <Hover>
+            {(hovering)=>(
+                <div
+                    style={styles.container}>
+                        {hovering === true && <div style={styles.tooltip}>{text}</div>}
+                        {children}
+                </div>
+            )}
+        </Hover>
     )
 }
 
@@ -38,7 +42,5 @@ function Tooltip({text, children, hovering}){
 
 Tooltip.propTypes = {
     text: PropTypes.string.isRequired
-    hovering:PropTypes.bool.isRequired
 }
 
-export default withHover(Tooltip)
